531057c5c3d87dfca158e0a252115ee90bc9b9fe
brenogcota <brenocota4@gmail.com>
2020-07-14 18:52:13 -0300

    finish aula 02 SignUp api presentation layer

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,8 +1,9 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missingparamserror'
 import { badRequest } from '../helpers/httphelper'
+import { Controller } from '../protocols/controller'
 
-export class SignUpController {
+export class SignUpController implements Controller {
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFileds = ['name', 'email', 'password', 'passwordConfirmation']
     for (const field of requiredFileds) {
