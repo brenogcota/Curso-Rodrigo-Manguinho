8fe1f6910378cd7bd2f3ed210dc21c174b3ea885
brenogcota <brenocota4@gmail.com>
2020-07-14 18:49:55 -0300

    finish aula 02 SignUp api presentation layer

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,11 +4,11 @@ import { badRequest } from '../helpers/httphelper'
 
 export class SignUpController {
   handle (httpRequest: HttpRequest): HttpResponse {
-    if (!httpRequest.body.name) {
-      return badRequest(new MissingParamError('name'))
-    }
-    if (!httpRequest.body.email) {
-      return badRequest(new MissingParamError('email'))
+    const requiredFileds = ['name', 'email', 'password', 'passwordConfirmation']
+    for (const field of requiredFileds) {
+      if (!httpRequest.body[field]) {
+        return badRequest(new MissingParamError(field))
+      }
     }
   }
 }
